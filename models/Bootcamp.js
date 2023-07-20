e24b30f6f11d4b5729e0ea4ac3f46f81c7e1eae8
UnfazedAy <ayomidesoniyi@gmail.com>
2023-07-20 15:05:56 +0100

    Modified the bootcamp model and implemented a PRE function

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -8,7 +9,7 @@ const BootcampSchema = new mongoose.Schema({
     trim: true,
     maxLength: [50, "Name can not be more than 50 characters"],
   },
-  slugify: String,
+  slug: String,
   description: {
     type: String,
     required: [true, "Please add a description"],
@@ -95,6 +96,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema)
